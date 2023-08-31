d2e3ebe9074ce2aaafb342df88a76fb9c007a5a7
Akhsan Ibrahim <akhsanibra08@gmail.com>
2023-08-31 13:40:39 +0700

    webcam resolution

--- main.py
+++ main.py
@@ -1,12 +1,31 @@
 import cv2
+import argparse
+
+def parse_arguments() -> argparse.Namespace:
+  parser = argparse.ArgumentParser(description="YOLOv8 live")
+  parser.add_argument(
+    "--webcam-resolution",
+    default=[1280,720],
+    nargs=2,
+    type=int
+  )
+  args = parser.parse_args()
+  return args
 
 def main():
+  args = parse_arguments()
+  frame_width, frame_height = args.webcam_resolution
+
   cap = cv2.VideoCapture(1)
+  cap.set(cv2.CAP_PROP_FRAME_WIDTH, frame_width)
+  cap.set(cv2.CAP_PROP_FRAME_HEIGHT, frame_height)
 
   while True:
     ret, frame = cap.read()
     cv2.imshow("yolov8", frame)
 
+    # print(frame.shape)
+    # break
     if (cv2.waitKey(30) == 27): # wait 30 ms for press Esc (27 ASCII)
       break
 
