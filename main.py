0ccc08a5b01144d62ac65f38c34a8a57f2236ce7
Akhsan Ibrahim <akhsanibra08@gmail.com>
2023-08-31 11:36:02 +0700

    show capture, exit with esc

--- main.py
+++ main.py
@@ -1,5 +1,14 @@
+import cv2
+
 def main():
-  print("Hello world!")
+  cap = cv2.VideoCapture(1)
+
+  while True:
+    ret, frame = cap.read()
+    cv2.imshow("yolov8", frame)
+
+    if (cv2.waitKey(30) == 27): # wait 30 ms for press Esc (27 ASCII)
+      break
 
 if __name__ == "__main__":
   main()
